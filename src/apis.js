7a69a56d894fb04ffdc1ec490ff4712c10bbcad5
Richard Nienaber <rjnienaber@gmail.com>
2020-12-02 21:15:36 +0000

    change in web page format

--- src/apis.js
+++ src/apis.js
@@ -4,24 +4,33 @@ const debug = require('debug')('apis');
 
 const GOV_UK_CHECKER_URL = 'https://www.gov.uk/find-coronavirus-local-restrictions';
 
-function parseHtmlForMessage(html) {
+function parseHtmlForDetails(html) {
   try {
     const $ = cheerio.load(html);
-    const element = $('#content > div > div > div > p:nth-child(5)');
-    return element[0].children[0].data.trim();
+
+    const tierElement = $('#content > div > div > div > div > h1');
+    const tierText = tierElement[0].children[0].data.trim();
+
+    const councilElement = $('#content > div > div > div > p:nth-child(2) > strong:nth-child(2)');
+    const council = councilElement[0].children[0].data.trim();
+    return [tierText, council];
   } catch (err) {
     debug('Failed to find message in postal code');
-    return '';
+    return [];
   }
 }
 
 async function checkPostalCode(postalCode) {
   const response = await axios.post(GOV_UK_CHECKER_URL, { 'postcode-lookup': postalCode });
 
-  const text = parseHtmlForMessage(response.data);
-  const matches = /(.*) will be in (.*)\./.exec(text);
+  const [tierText, council] = parseHtmlForDetails(response.data);
+  if (!tierText || !council) {
+    return undefined;
+  }
+
+  const matches = /This area is in (.*)/.exec(tierText);
   if (matches) {
-    const [, council, tier] = matches;
+    const [, tier] = matches;
     return { council, tier };
   }
   return undefined;

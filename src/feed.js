51623b9fb3b6b25c3c197493c047617fa40167bf
Richard Nienaber <rjnienaber@gmail.com>
2020-12-08 01:03:09 +0000

    generate rss feed for changes

--- src/feed.js
+++ src/feed.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const {promises: fs} = require('fs');
+const { Feed } = require('feed');
+const { promises: fs } = require('fs');
 
 function generateFeedFilePath(dataDir, locations) {
   const postalCodes = locations.map((l) => l.postalCode.toLowerCase());
@@ -36,8 +37,49 @@ async function updateFeedFileTime(feedFilePath) {
   await fs.utimes(feedFilePath, now, now);
 }
 
+function generateFeedFile(feedFilePath, logs, config) {
+  const now = new Date();
+  const postalCodes = logs.map((l) => l.location.postalCode);
+  const link = `${config.appUrl}/rss.xml`;
+
+  const feed = new Feed({
+    title: 'Tier Changelog',
+    description: `The list of covid-19 tier changes for the following postal codes: ${postalCodes.join(', ')}`,
+    link,
+    lastBuildDate: now,
+    generator: 'https://github.com/rjnienaber/tiersforbeers',
+    copyright: 'Copyright (C) 2020 Richard Nienaber',
+    feedLinks: {
+      rss: link,
+    },
+  });
+
+  logs.forEach((log) => {
+    const {
+      id,
+      createdAt,
+      location: { name, postalCode, tier, council },
+    } = log;
+    const title = `Change detected for area '${name}' (${postalCode}): ${tier}`;
+    const description =
+      `A tier change has been detected for area '${name}' (${postalCode}), ` +
+      `in the council '${council}'. The new tier is '${tier}'`;
+
+    feed.addItem({
+      title,
+      link: config.govUk.url,
+      id: `${config.appUrl}/${id}`,
+      date: createdAt,
+      description,
+    });
+  });
+
+  return feed.rss2();
+}
+
 module.exports = {
+  generateFeedFile,
   generateFeedFilePath,
   getFeedFileTime,
-  updateFeedFileTime
+  updateFeedFileTime,
 };
--- test/feed.test.js
+++ test/feed.test.js
@@ -1,12 +1,11 @@
-const {updateFeedFileTime} = require('../src/feed')
-const {getFeedFileTime} = require('../src/feed')
 const chai = require('chai');
 const { expect } = require('chai');
-const {promises: fs} = require('fs');
-const { generateFeedFilePath } = require('../src/feed');
-const {file } = require('tmp-promise');
+const { promises: fs } = require('fs');
+const { file } = require('tmp-promise');
 const chaiDateTime = require('chai-datetime');
-
+const config = require('../src/config');
+const { createDatabase } = require('../src/database');
+const { generateFeedFilePath, getFeedFileTime, generateFeedFile, updateFeedFileTime } = require('../src/feed');
 
 chai.use(chaiDateTime);
 
@@ -27,31 +26,31 @@ describe('feed', () => {
 
     beforeEach(async () => {
       const tmpFile = await file();
-      cleanup = tmpFile.cleanup
+      cleanup = tmpFile.cleanup;
       path = tmpFile.path;
-    })
+    });
 
     afterEach(async () => {
       if (cleanup) {
         await cleanup();
       }
-    })
+    });
 
     describe('#getFeedFileTime', () => {
       it('returns "current" when made today', async () => {
         const result = await getFeedFileTime(path);
         expect(result).to.equal('current');
-      })
+      });
 
       it('returns "outdated" when older than today', async () => {
         const now = new Date();
         const yesterday = new Date(now);
         yesterday.setDate(yesterday.getDate() - 1);
-        await fs.utimes(path, yesterday, yesterday)
+        await fs.utimes(path, yesterday, yesterday);
 
         const result = await getFeedFileTime(path);
         expect(result).to.equal('outdated');
-      })
+      });
 
       it('returns "absent" when missing', async () => {
         await cleanup();
@@ -59,14 +58,14 @@ describe('feed', () => {
 
         const result = await getFeedFileTime(path);
         expect(result).to.equal('absent');
-      })
-    })
+      });
+    });
 
     it('#updateFeedFileTime', async () => {
       const now = new Date();
       const yesterday = new Date(now);
       yesterday.setDate(yesterday.getDate() - 1);
-      await fs.utimes(path, yesterday, yesterday)
+      await fs.utimes(path, yesterday, yesterday);
 
       await updateFeedFileTime(path);
 
@@ -74,5 +73,61 @@ describe('feed', () => {
       expect(stat.atime).to.be.closeToTime(now, 1);
       expect(stat.mtime).to.be.closeToTime(now, 1);
     });
-  })
+
+    describe('#generateFeedFile', () => {
+      let db;
+      beforeEach(async () => {
+        db = await createDatabase();
+      });
+
+      afterEach(async () => {
+        if (db) {
+          await db.close();
+        }
+      });
+
+      it('generates xml file', async () => {
+        db = await createDatabase();
+        const holmes = {
+          name: 'Sherlock Holmes',
+          postalCode: 'NW16XE',
+          council: 'City of Westminster',
+          tier: 'Tier 2: High alert',
+        };
+        const locations = await db.locations.updateLocations([holmes]);
+        await db.logs.updateLogs(locations);
+        const latestLogs = await db.logs.latest(holmes.postalCode);
+
+        const testConfig = { ...config, appUrl: 'http://localhost:8000' };
+
+        const now = new Date();
+        const xml = await generateFeedFile(path, latestLogs, testConfig);
+        const getElement = (tag) => new RegExp('<' + tag + '>(.+)</' + tag + '>').exec(xml)[1];
+
+        // channel
+        expect(xml).to.contains('<title>Tier Changelog</title>');
+        expect(xml).to.contains('<link>http://localhost:8000/rss.xml</link>');
+        expect(new Date(getElement('lastBuildDate'))).to.be.closeToTime(now, 1);
+        expect(xml).to.contains('<generator>https://github.com/rjnienaber/tiersforbeers</generator>');
+        expect(xml).to.contains('<copyright>Copyright (C) 2020 Richard Nienaber</copyright>');
+        expect(xml).to.contains(
+          '<description>The list of covid-19 tier changes for the following postal codes: NW16XE</description>',
+        );
+        expect(xml).to.contains(
+          '<atom:link href="http://localhost:8000/rss.xml" rel="self" type="application/rss+xml"/>',
+        );
+
+        // item
+        expect(xml).to.contains(
+          '<title><![CDATA[Change detected for area \'Sherlock Holmes\' (NW16XE): Tier 2: High alert]]></title>'
+        );
+        expect(xml).to.contains(`<link>${config.govUk.url}</link>`);
+        expect(xml).to.contains(`<guid>http://localhost:8000/1</guid>`);
+        expect(new Date(getElement('pubDate'))).to.be.closeToTime(now, 1);
+        expect(xml).to.contains(
+          `<description><![CDATA[A tier change has been detected for area 'Sherlock Holmes' (NW16XE), in the council 'City of Westminster'. The new tier is 'Tier 2: High alert']]></description>`,
+        );
+      });
+    });
+  });
 });

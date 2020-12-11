b5f6e62571c8071398719c5ecbe2216587495f0c
Richard Nienaber <rjnienaber@gmail.com>
2020-12-11 00:12:14 +0000

    generate appUrl correctly when on glitch

--- src/config.js
+++ src/config.js
@@ -3,10 +3,17 @@ const path = require('path');
 const dataDir = path.resolve(path.join(__dirname, '..', '.data'));
 const databaseFilePath = path.join(dataDir, 'db.sqlite');
 
+let appUrl = '';
+
+// use project name from glitch to get hosted url
+if (process.env.PROJECT_DOMAIN) {
+  appUrl = `https://${process.env.PROJECT_DOMAIN}.glitch.me`
+}
+
 const config = {
   dataDir,
   databaseFilePath,
-  appUrl: process.env.PROJECT_DOMAIN,
+  appUrl,
   govUk: {
     url: 'https://www.gov.uk/find-coronavirus-local-restrictions',
   },
